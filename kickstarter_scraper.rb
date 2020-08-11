94ce39c7df17a8d2f6bf1c3125c19387ee03af81
Christopher Dutcher <cadutcher@icloud.com>
2020-08-11 12:32:30 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,8 @@
 # require libraries/modules here
-require
+require "nokogiri"
+require 'pry'
 def create_project_hash
   html.File.read('fixtures/kickstarter.html')
-
   kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
